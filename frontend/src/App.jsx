731e549c70a61b64db4cada301b57a3ea9a722f6
AzrinHakimi88 <azrinhakimi630@gmail.com>
2025-02-26 14:51:57 +0800

    Login and Signup page setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from "react-router"
 import LandingPage from "./pages/LandingPage"
-
+import LoginPage from "./pages/LoginPage"
+import SignUpPage from "./pages/SignUpPage"
 
 function App() {
 
   return (
     <Routes>
       <Route index element={<LandingPage/>}/>
+      <Route path={"/login"} element={<LoginPage/>}/>
+      <Route path={"/signup"} element={<SignUpPage/>}/>
     </Routes>
   )
 }
--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { NavLink } from 'react-router'
 
 const LandingPage = () => {
   return (
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <div className="flex gap-8 items-center justify-center h-screen bg-gray-100">
+        <NavLink to={"/signup"} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer">
+            Sign Up
+        </NavLink>
+
+        <NavLink to={"/login"} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer">
+            Login
+        </NavLink>
+    </div>
   )
 }
 
